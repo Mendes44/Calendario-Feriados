e093f1833b1c978d742965b929593e7634f79a6e
Mendes44 <marcosmendesm10@gmail.com>
2026-07-10 10:52:39 -0300

    Finalizando Camada De Serviço

--- retornaFeriado.js
+++ retornaFeriado.js
@@ -5,4 +5,20 @@ import { formataDiaSemana, formataDataFeriado } from "./formataDataFeriado.js";
 
 export function retornaProximosFeriados (numeroFeriados){
     
+    let feriadosFiltro = filtraFeriados(feriadosNacionais, numeroFeriados);
+    let feriadosFiltroFormatado = []; 
+
+    feriadosFiltro.forEach(feriado => {
+        const dataFeriado = feriado.data;
+        let diaDaSemana = formataDiaSemana(dataFeriado);
+        const dataFeriadoFormatada = formataDataFeriado(dataFeriado);
+
+        feriadosFiltroFormatado.push({
+            nome: feriado.nome,
+            diaDaSemana: diaDaSemana,
+            data: dataFeriadoFormatada
+        });
+    });
+
+    return feriadosFiltroFormatado;
 }
