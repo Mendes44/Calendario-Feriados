ce829db33853c43f1c7123d86130de14a4b6f141
Mendes44 <marcosmendesm10@gmail.com>
2026-07-10 11:11:00 -0300

    Alerando a entrada de dados para manuel

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //Camada de Exibição
 import { retornaProximosFeriados } from "./retornaFeriado.js";
 import imprimeFeriados from "./imprimeFeriado.js";
+import entradaDados from 'readline-sync'
 
 let data = new Date();
 data = data.getFullYear();
-let numeroFeriados = 3;
+let numeroFeriados = Number(entradaDados.question('Digite o número de feriado que deseja saber no ano de ' + data + ': '));
 
 console.log(`       :::Proximos Feriados do Ano de ${data}:::  \n   `);
 
