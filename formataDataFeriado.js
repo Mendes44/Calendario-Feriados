50b6ed848023df551a0fd3823bf870af53f25aaa
Mendes44 <marcosmendesm10@gmail.com>
2026-07-10 10:41:14 -0300

    Criando formataDataFeriado

--- formataDataFeriado.js
+++ formataDataFeriado.js
@@ -1,4 +1,11 @@
 //Subcamada responsavel por formatação das datas dos feriados, Seu objetivo e retornar data e dia da semana formatados corretamente.
 //Lembrando que o JS usa o formato americano de data mm/dd.
 import feriadosNacionais from "./dataFeriado.js";
+import { filtraFeriados } from "./filtros.js";
+
+//Aqui estou buscando o dia e usando como indice para pegar o nome da semana no array que crei.
+function formataDiaSemana (dataFeriado){
+    let diaDaSemana = ['Segunda', 'Terca', 'Quarta', 'Quinta', 'Sexta', 'Sabado','Domingo'];
+    return diaDaSemana[dataFeriado.getDay()];
+}
 
